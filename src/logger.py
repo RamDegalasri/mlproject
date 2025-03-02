064505ba86ec9ecbeea65a6802750c7c100a6bda
Ram Degalasri <ram.allen2017@gmail.com>
2025-03-01 19:15:33 -0500

    EDA and Problem Statement

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok = True)
-print(os.listdir(logs_path))
+# print(os.listdir(logs_path))
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
@@ -16,6 +16,3 @@ logging.basicConfig(
 
 
 )
-
-if __name__=="__main__":
-    logging.info("Logging has started")
